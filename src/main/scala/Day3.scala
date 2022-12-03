b6224f0724349fd9f49ce817ba43422de6b4a347
Joe Carroll <josephcarroll@users.noreply.github.com>
2022-12-03 09:59:04 -0500

    tidy up of day 3

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -2,20 +2,25 @@ package joe.aoc
 
 object Day3 extends App {
 
-  println("day 3!")
   val items = Helpers.readInput("day3-input.txt")
+
+  def toSet(input: String): Set[Char] = Set(input.toCharArray: _*)
+  def priorityOf(input: Char): Int = {
+    val asChar = input.asInstanceOf[Int] // ASCII
+    if (input.isLower) asChar - 96 else asChar - 38
+  }
 
   val priorities = items.map { rucksack =>
     val (left, right) = rucksack.splitAt(rucksack.length / 2)
-    val overlap = Set(left.toCharArray: _*).intersect(Set(right.toCharArray: _*)).head
-    if (overlap.isLower) overlap.asInstanceOf[Int] - 96 else overlap.asInstanceOf[Int] - 38
+    val overlap = toSet(left).intersect(toSet(right)).head
+    priorityOf(overlap)
   }
 
   println(priorities.sum)
 
   val badges = items.grouped(3).map { case Seq(one, two, three) =>
-    val overlap = Set(one.toCharArray: _*).intersect(Set(two.toCharArray: _*)).intersect(Set(three.toCharArray: _*)).head
-    if (overlap.isLower) overlap.asInstanceOf[Int] - 96 else overlap.asInstanceOf[Int] - 38
+    val overlap = toSet(one).intersect(toSet(two)).intersect(toSet(three)).head
+    priorityOf(overlap)
   }
 
   println(badges.sum)
