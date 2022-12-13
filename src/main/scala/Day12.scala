dcac5ba486b5b9dcda5e62b7ff0427613651e16b
Joe Carroll <josephcarroll@users.noreply.github.com>
2022-12-13 18:28:55 -0500

    day 12 clean up

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -28,14 +28,10 @@ object Day12 extends App {
     }
   }
 
-  def weightOf(input: Node): Int = {
-    if (input.value == 'S') {
-      'a'.toInt
-    } else if (input.value == 'E') {
-      'z'.toInt
-    } else {
-      input.value.toInt
-    }
+  def weightOf: Node => Int = {
+    case Node(_, _, 'S') => 'a'.toInt
+    case Node(_, _, 'E') => 'z'.toInt
+    case other => other.value.toInt
   }
 
   for (i <- lines.indices) {
@@ -45,22 +41,25 @@ object Day12 extends App {
       val down = if (j + 1 < lines.head.length) Some(Node(i, j + 1)) else None
       val left = if (i - 1 >= 0) Some(Node(i - 1, j)) else None
       val right = if (i + 1 < lines.length) Some(Node(i + 1, j)) else None
-      val reachable = Seq(up, down, left, right).flatten
+      val neighbours = Seq(up, down, left, right).flatten
 
-      reachable.foreach { candidateNode =>
+      val reachable = neighbours.flatMap { candidateNode =>
         val weight = weightOf(candidateNode) - weightOf(currentNode)
         if (weight == 1 || weight <= 0) {
-          val edge = DiEdge(currentNode, candidateNode)
-          graph.add(edge)
-        }
+          Some(DiEdge(currentNode, candidateNode))
+        } else None
       }
+      reachable.foreach(graph.add)
     }
   }
 
   val path = graph.get(start).shortestPathTo(graph.get(end))
   println(path.get.edges.size)
 
-  val candidates = for {i <- lines.indices; j <- lines.head.indices} yield {
+  val candidates = for {
+    i <- lines.indices
+    j <- lines.head.indices
+  } yield {
     val candidate = Node(i, j)
     if (Set('a', 'S').contains(candidate.value)) {
       val path = graph.get(candidate).shortestPathTo(graph.get(end))
