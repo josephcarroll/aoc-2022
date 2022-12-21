d4d860c04821f0f35486e25ca3c44c2b235e9195
Joe Carroll <josephcarroll@users.noreply.github.com>
2022-12-21 13:34:25 -0500

    small 18 clean up

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -1,5 +1,7 @@
 package joe.aoc
 
+import scala.collection.mutable
+
 object Day18 extends App {
 
   case class Voxel(x: Int, y: Int, z: Int)
@@ -32,12 +34,30 @@ object Day18 extends App {
     voxel.copy(x = voxel.x, y = voxel.y - 1, z = voxel.z),
     voxel.copy(x = voxel.x, y = voxel.y, z = voxel.z + 1),
     voxel.copy(x = voxel.x, y = voxel.y, z = voxel.z - 1)
-  )
+  ).filter(v => v.x >= -1 && v.y >= -1 && v.z >= -1)
+    .filter(v => v.x < 35 && v.y < 35 && v.z < 35)
 
   val neighbourCount = voxels.toSeq.map { voxel =>
     val possibleNeighbours = neighboursOf(voxel)
     6 - possibleNeighbours.count(voxels.contains)
   }
   println(neighbourCount.sum)
+
+  val air = new mutable.HashSet[Voxel]()
+  def dfs(location: Voxel): Unit = {
+    if (!air.contains(location)) {
+      air.add(location)
+      val notLava = neighboursOf(location).filterNot(voxels.contains)
+      notLava.foreach(dfs)
+    }
+  }
+
+  dfs(Voxel(0, 0, 0))
+
+  val touchingAir = voxels.toSeq.map { voxel =>
+    val possibleNeighbours = neighboursOf(voxel)
+    possibleNeighbours.count(air.contains)
+  }
+  println(touchingAir.sum)
 
 }
